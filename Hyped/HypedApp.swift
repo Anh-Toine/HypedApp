78bc62b3440d5520ad746341781ec0ce2419eae4
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-10-04 10:05:44 -0400

    Initial Commit

---
+++ Hyped/HypedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedApp.swift
+//  Hyped
+//
+//  Created by Lukas Nguyen on 2022-10-04.
+//
+
+import SwiftUI
+
+@main
+struct HypedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
